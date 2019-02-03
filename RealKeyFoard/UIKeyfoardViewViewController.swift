7ab23ac81926e52fac1d50607f2a82258bd3d46f
배지영 <melissaeliza0@gmail.com>
2019-02-03 19:17:58 +0900

    특수키 컨트롤러 세팅

--- RealKeyFoard/UIKeyfoardViewViewController.swift
+++ RealKeyFoard/UIKeyfoardViewViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class UIKeyfoardViewViewController: UIInputViewController, UIKeyForceViewDelegate {
 
-    var keyfoardView: EmKeyboardView?
+//    var keyfoardView: EmKeyboardView?
+    var keyforardView: UIKeyfoardView?
     private var currentText: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setKeyfoardView()
+    }
+
+    func setKeyfoardView() {
+        guard let keyboardView = Bundle.main.loadNibNamed("UIKeyfoardViewViewController", owner: self, options: nil)?[1] as? UIKeyfoardView else { return }
+        keyboardView.delegate = self
+        inputView = keyboardView
+    }
+
+    func setTestView() {
         guard let keyboardView = Bundle.main.loadNibNamed("UIKeyfoardViewViewController", owner: self, options: nil)?.first as? EmKeyboardView else { return }
         keyboardView.delegate = self
         inputView = keyboardView
+    }
+
+    func onTouchKeyCap(_ keycap: KeyCap) {
+        print(keycap)
     }
 
     func onTouchButton(_ text: String) {
